860b0afb206a9ffa451cfe7c1a7fca61eb4ad6f0
lgy <happygy@wwway.biz>
2021-12-20 18:48:50 +0900

    음식 수정

--- src/components/food/Food.jsx
+++ src/components/food/Food.jsx
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 function Food({name, rating, img}) {
   return (
     <div>
-      <h3>like {name} ({rating} / 5.0)</h3>
+      <h3>{name} ({rating} / 5.0)</h3>
       <img src={img} alt={name}></img>
     </div>
   );
